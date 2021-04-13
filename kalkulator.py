08f80052fe295200ea1ab697e4d0e3ddd451356d
Piotr Szczygieł <you@example.com>
2021-04-13 19:37:24 +0200

    ogólny zapis

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,11 @@
+
+def get_info():
+print("Witaj, to jest prosty kalkulator!")
 def dodaj():
-print('wprowadź dwie liczby, a zostaną one dodane. sprawdzam czy działa ')
+	print('wprowadź dwie liczby, a zostaną one dodane. sprawdzam czy działa ')
 	a = int(intput())
 	b = int(input())
 	print(a + b)
 	
+	get_info()
 	dodaj()
